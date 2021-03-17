08c2ec22f7ebd54730b916c414770cf01af2589d
Reoul18 <rebeccaleonora18@gmail.com>
2021-03-17 03:19:20 +0100

    Progress 1
    Animation is added
    JS en CSS are tidied up
    Some new contents added in HTML

--- script.js
+++ script.js
@@ -17,20 +17,8 @@ function initClock() {
 function renderClock() {
     var clock = document.getElementById('klok');
     var time  = getCurrentTime(new Date());
-    var sep   = flashSeperator(time['seconds']);
 
-    clock.innerHTML = time['hours'] + sep +  time['minutes'] + sep + time['seconds'];
-}
-
-function flashSeperator(seconds) {
-	var sepClass = '';
-
-	//toggle
-	if (seconds % 3 === 1) {
-		sepClass = ' class="puntjes"';
-	}
-
-	return '<span' + sepClass + '">:</span>';
+    clock.innerHTML = time['hours'] + ':' +  time['minutes'] + ':' + time['seconds'];
 }
 
 function getCurrentTime(date) {
@@ -58,6 +46,11 @@ function getCurrentTime(date) {
 initClock();
 
 
+//VANDAAG
+var today = new Date();
+var dag = ['Maandag', 'Dinsdag', 'Woensdag', 'Dondergdag', 'Vrijdag', 'Zaterdag', 'Zondag'];
+
+document.getElementById('vandaag').innerHTML = dag[today.getDay()-1];
 
 
 //SPENT DAYS
@@ -72,18 +65,15 @@ var days = Math.abs((now.getTime() - vertrekDatum.getTime()) / day);
 document.getElementById('dagen').innerHTML = Math.round(days);
 }
 
-// start
 showDaysSpend();
 
 
-
-
 //DATUM
-var today = new Date();
+var vandaag = new Date();
 var maanden = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-var jaar = today.getFullYear();
+var jaar = vandaag.getFullYear();
 
-document.getElementById('datum').innerHTML = today.getDate() + ' ' + maanden[today.getMonth()] + ' ' + jaar;
+document.getElementById('datum').innerHTML = vandaag.getDate() + ' ' + maanden[vandaag.getMonth()] + ' ' + jaar;
 
 
 
@@ -95,3 +85,19 @@ var difference = aardeTijd.getTime() - marsTijd.getTime();
 var diffDays = difference / 1000 / 60 / 60 / 24 ;
 
 document.getElementById('dagenVerschil').innerHTML = diffDays;
+
+//Temperatuur
+var temp = new Date();
+var uur = temp.getHours();
+var temperatuur = document.getElementById('temperatuur');
+if(uur >= 0 && uur < 6){  
+	temperatuur.innerHTML='-60 &#8451'; //temperatuur voor 6u in de ochtend
+} else if (uur >= 6 && uur < 12){
+	temperatuur.innerHTML='-52 &#8451'; // temperatuur voor 12u in de middag
+} else if (uur >= 12 && uur < 18){
+	temperatuur.innerHTML='-53 &#8451'; // temperatuur voor 6u in de middag
+} else if (uur >= 18){
+	temperatuur.innerHTML='-51 &#8451'; // tempertuur na 6u in de middag
+}
+
+
